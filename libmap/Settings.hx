db59fc1d69575d32385a6d2f8f5e87a99d43fc6a
progsy <213481401+progsy@users.noreply.github.com>
2026-07-11 18:37:47 +0200

    Add map format to settings

--- libmap/Settings.hx
+++ libmap/Settings.hx
@@ -1,12 +1,18 @@
 package libmap;
 
+enum Format {
+	Quake;
+	Quake2;
+}
+
 class Settings {
 	/** 
 		Used to scale down the entities.
 		Ideally this should be set to 32 (like in Doom), where each 32 Trenchbroom units represent one meter.
-		For the sake of compatibilty this is set to 1.
 	**/
-	public static var unitsPerMeter:Int = 1 /*32*/;
+	public static var unitsPerMeter:Int = 32;
+
+	public static var format:Format = Quake2;
 
 	public static inline function scale(v:Float):Float {
 		return v / unitsPerMeter;
